d958e89c7c2691dfb377445825b4d63d7f793b82
Hazel Wu <tinazx056@gmail.com>
2019-04-05 22:52:02 +0800

    [Add] 取得單一產品

--- src/components/pages/CustomerOrder.vue
+++ src/components/pages/CustomerOrder.vue
@@ -16,12 +16,37 @@ div
                             del.h6(v-if="item.price") 原價 {{item.origin_price}} 元
                             .h5(v-if="item.price") 現在只要 {{item.price}} 元
                         .card-footer.d-flex
-                        button.btn.btn-outline-secondary.btn-sm(type='button')
-                            i.fas.fa-spinner.fa-spin 
+                        button.btn.btn-outline-secondary.btn-sm(type='button', @click="getProduct(item.id)")
+                            i.fas.fa-spinner.fa-spin(v-if="status.loadingItem === item.id")
                             | 查看更多
                         button.btn.btn-outline-danger.btn-sm.ml-auto(type='button')
-                            i.fas.fa-spinner.fa-spin 
+                            i.fas.fa-spinner.fa-spin(v-if="status.loadingItem === item.id")
                             | 加到購物車
+    pagination(:pages="pagination", @getPage="getProducts")
+    // 查看單一產品 Modal
+    #productModal.modal.fade(tabindex='-1', role='dialog', aria-labelledby='exampleModalLabel', aria-hidden='true')
+        .modal-dialog.modal-lg(role='document')
+            .modal-content.border-0
+                .modal-header.bg-dark.text-white 
+                    h5#exampleModalLabel.modal-title {{product.title}}
+                    button.close(type='button', data-dismiss='modal', aria-label='Close')
+                    span(aria-hidden='true') ×
+                .modal-body
+                    img.img-fluid(:src="product.imageUrl")
+                    blockquote.blockquote.mt-3
+                        p.mb-0 {{product.content}}
+                        footer.blockquote-footertext-right {{product.description}}
+                    .d-flex.justify-content-between.align-items-baseline
+                        .h4(v-if="!product.price") {{product.origin_price }} 元
+                        del.h6(v-if="product.price") 原價 {{product.origin_price }} 元
+                        .h4(v-if="product.price") 現在只要 {{product.price }} 元
+                    select.form-control.mt-3(v-model="product.num")
+                        option(value="1") 選購 1 件
+                .modal-footer
+                    .text-muted.text-nowrap.mr-3
+                        | 小計
+                        strong {{product.num * product.price }} 元
+                    button.btn.btn-primary(type='button') 加到購物車
 
 </template>
 
@@ -35,12 +60,16 @@ export default {
     data() {
         return {
             products: [],
+            product: {},
             isLoading: false,
-            pagination: {}
+            pagination: {},
+            status: {
+                loadingItem: '', // 存放產品 id
+            }
         };
     },
     methods: {
-        getCustomerOrder(page = 1) {
+        getProducts(page = 1) {
             const vm = this;
             const api = `${process.env.APIPATH}/api/${
                 process.env.CUSTOMPATH
@@ -51,11 +80,25 @@ export default {
                 vm.isLoading = false;
                 vm.products = response.data.products;
                 vm.pagination = response.data.pagination;
+                console.log(vm.pagination)
+            });
+        },
+        getProduct(id) { // 取得單一產品
+            const vm = this;
+            const api = `${process.env.APIPATH}/api/${
+                process.env.CUSTOMPATH
+            }/product/${id}`;
+            vm.status.loadingItem = id;
+            this.$http.get(api).then(response => {
+                console.log(response.data);
+                vm.status.loadingItem = '';
+                vm.product = response.data.product;
+                $('#productModal').modal('show');
             });
         }
     },
     created() {
-        this.getCustomerOrder();
+        this.getProducts();
     }
 };
 </script>
