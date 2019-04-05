52679b29b389c15ed86562a862391ec02d1dd7c0
Hazel Wu <tinazx056@gmail.com>
2019-04-05 17:53:18 +0800

    [Add] SideBar加入登出功能

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,24 @@
-<template>
-  <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
-      </ul>
-  </nav>
+<template lang="pug">
+div
+    nav.navbar.navbar-dark.fixed-top.bg-dark.flex-md-nowrap.p-0.shadow
+        a.navbar-brand.col-sm-3.col-md-2.mr-0(href='#') BookStore
+        input.form-control.form-control-dark.w-100(type='text', placeholder='Search', aria-label='Search')
+        ul.navbar-nav.px-3
+            li.nav-item.text-nowrap
+            a.nav-link(@click='signout') 登出
 </template>
 <script>
 export default {
-  data() {
-    return {
-
+    data() {
+        return {};
+    },
+    methods: {
+        signout() {
+            const api = `${process.env.APIPATH}/logout`;
+            this.$http.post(api).then(response => {
+                response.data.success ? this.$router.push("/login") : false;
+            });
+        }
     }
-  },
-  methods: {
-    signout() {
-      const api = `${process.env.APIPATH}/logout`;
-      this.$http.post(api)
-        .then(response => {
-          response.data.success ? this.$router.push('/login') : false
-        })
-    }
-  }
-}
+};
 </script>
